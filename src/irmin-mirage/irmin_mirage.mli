ae59d9793544e653a9fa49372e82915421016429
Hannes Mehnert <hannes@mehnert.org>
2019-03-02 22:35:41 +0100

    remove irmin-graphql from irmin-mirage for now (depends on Str, not available in MirageOS)

--- src/irmin-mirage/irmin_mirage.mli
+++ src/irmin-mirage/irmin_mirage.mli
@@ -168,24 +168,3 @@ module Git: sig
     module KV_RW (C: Mirage_clock.PCLOCK): KV_RW with type git := G.t and type clock := C.t
   end
 end
-
-module Graphql: sig
-  module type S = sig
-    module Pclock: Mirage_clock_lwt.PCLOCK
-    module Http: Cohttp_lwt.S.Server
-    module Store: Irmin.S with type Private.Sync.endpoint = Git_mirage.endpoint
-
-    val start:
-      pclock:Pclock.t
-      -> http:(Http.t -> unit Lwt.t)
-      -> Store.t -> unit Lwt.t
-  end
-
-  module Make
-      (Http: Cohttp_lwt.S.Server)
-      (Store: Irmin.S with type Private.Sync.endpoint = Git_mirage.endpoint)
-      (Pclock: Mirage_clock_lwt.PCLOCK):
-    S with module Pclock = Pclock
-      and module Store = Store
-      and module Http = Http
-end
