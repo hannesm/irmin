a4b36000fc530fdb2f9893542812dc9dbc507d30
Hannes Mehnert <hannes@mehnert.org>
2019-04-07 00:45:06 +0200

    mirage no graphql

--- src/irmin-mirage/irmin_mirage.mli
+++ src/irmin-mirage/irmin_mirage.mli
@@ -195,31 +195,3 @@ module Git : sig
       KV_RW with type git := G.t and type clock := C.t
   end
 end
-
-module Graphql : sig
-  module Server: sig
-    module type S = sig
-      module Pclock : Mirage_clock_lwt.PCLOCK
-
-      module Http : Cohttp_lwt.S.Server
-
-      module Store :
-        Irmin.S with type Private.Sync.endpoint = Git_mirage.endpoint
-
-      val start :
-        pclock:Pclock.t ->
-        http:(Http.t -> unit Lwt.t) ->
-        Store.repo ->
-        unit Lwt.t
-    end
-
-    module Make
-        (Http : Cohttp_lwt.S.Server)
-        (Store : Irmin.S with type Private.Sync.endpoint = Git_mirage.endpoint)
-        (Pclock : Mirage_clock_lwt.PCLOCK) :
-      S
-      with module Pclock = Pclock
-       and module Store = Store
-       and module Http = Http
-  end
-end
